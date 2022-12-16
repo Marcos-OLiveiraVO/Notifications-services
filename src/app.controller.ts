cd4238816bffb14ff65a2f550839041d89fb35bd
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2022-12-16 20:50:00 -0300

    notications-service

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Body } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Post } from '@nestjs/common/decorators/http/request-mapping.decorator';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
